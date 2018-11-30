e3b8e1614e91c292b866bf8ea87952ad0ec6c807
Brandon Redder <BrandonR@gatech.edu>
2018-11-29 20:13:35 -0500

    Moved old files to archive folder

--- Archive/historicalTweetProcessing.py
+++ Archive/historicalTweetProcessing.py
@@ -1,7 +1,6 @@
 import sys
-import re
 import json
-import pymysql
+import boto3
 from datetime import datetime, date, time, timedelta
 import dateutil.parser as parser
 import multiprocessing as mp
@@ -14,11 +13,13 @@ from tweepy.streaming import StreamListener
 json_data=open("config.json").read()
 config = json.loads(json_data)
 
-#DB Credentials
-USERNAME = config['RDS']['USERNAME']
-PASSWORD = config['RDS']['PASSWORD']
-DB_NAME = config['RDS']['DB_NAME']
-HOST = config['RDS']['HOST']
+AWS_KEY = config['AWS']['AWS_KEY']
+AWS_SECRET = config['AWS']['AWS_SECRET']
+REGION = config['AWS']['REGION']
+
+sqs = boto3.resource('sqs', aws_access_key_id=AWS_KEY,
+                            aws_secret_access_key=AWS_SECRET,
+                            region_name=REGION)
 
 #Twitter API Keys
 access_token = config['tweepy']['access_token']
@@ -53,42 +54,9 @@ else:
     tweets_begin = datetime.combine(date.today() - timedelta(days=1), time.min)
     tweets_end = datetime.combine(date.today() - timedelta(days=1), time.max)
 
-TERMS={}
-
-#-------- Load Sentiments Dict ----
-sent_file = open('AFINN-111.txt')
-sent_lines = sent_file.readlines()
-for line in sent_lines:
-	s = line.split("\t")
-	TERMS[s[0]] = s[1]
-
-sent_file.close()
-
-#-------- Find Sentiment  ----------
-def findsentiment(tweet):
-    sentiment=0.0
-
-    if tweet.has_key('full_text'):
-        text = tweet['full_text']		
-        text=re.sub('[!@#$)(*<>=+/:;&^%#|\{},.?~`]', '', text)
-        splitTweet=text.split()
-
-        for word in splitTweet:
-            if TERMS.has_key(word):
-                sentiment = sentiment+ float(TERMS[word])
-
-    return sentiment
-
-timestamp_f = '%Y-%m-%d %H:%M:%S'
-
 def produceTweets(id):
-    conn = pymysql.connect (host = HOST,
-                user = USERNAME,
-                passwd = PASSWORD,
-                db = DB_NAME, 
-		        port = 3306, charset='utf8mb4', use_unicode=True)
-    
-    cursor = conn.cursor()
+    # Get the queue
+    queue = sqs.get_queue_by_name(QueueName='Pending_Tweets')
 
     items = twitter_api.user_timeline(user_id = id, tweet_mode="extended", wait_on_rate_limit=True)
     while(True):
@@ -99,33 +67,13 @@ def produceTweets(id):
             for i in items:
                 if tweets_begin <= i.created_at <= tweets_end:
                     #print json.dumps(i._json)
-                    #print "@%s (%s) '%s'" % (i.user.screen_name, str(i.created_at), i.full_text)
-                    
-                    data = json.loads(json.dumps(i._json))
-
-                    handle = data['user']['screen_name']
-                    tweet = data['full_text']
-                    date_time = parser.parse(data['created_at'])
-        
-                    sentiment = findsentiment(data)
-                    
-                    statement = "SELECT * FROM tweets WHERE twitter_handle='%s' AND date_time='%s';" % (handle.replace("'","''"), date_time.strftime(timestamp_f))
-                    print statement
-                    cursor.execute(statement)
-                    
-                    if cursor.fetchone() == None:
-                        statement = "INSERT INTO tweets (twitter_handle, date_time, tweet_text, sentiment_score) VALUES ('%s', '%s', '%s', %f);" % (handle.replace("'","''"), date_time.strftime(timestamp_f),tweet.replace("'","''"), sentiment)
-                        print statement
-                        cursor.execute(statement)
+                    print "@%s (%s) '%s'" % (i.user.screen_name, str(i.created_at), i.full_text)
+                    queue.send_message(MessageBody=json.dumps(i._json))
         
         items = twitter_api.user_timeline(user_id = id, tweet_mode="extended", max_id = items[-1].id, wait_on_rate_limit=True)
-    
-    conn.commit()
-    cursor.close()
-    conn.close() 
 
 #Setup multiprocessing pool
-tweet_pool = mp.Pool(processes=20)
+tweet_pool = mp.Pool(processes=10)
 
 def main():
     #produceTweets(user_ids[5])
